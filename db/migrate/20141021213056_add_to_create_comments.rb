47d65f5e045d1eb1a9b22ae693c9efbfb8701226
Denzyl Balram <denzyl.balram@gmail.com>
2014-10-21 15:50:06 -0700

    First commit

---
+++ db/migrate/20141021213056_add_to_create_comments.rb
@@ -0,0 +1,9 @@
+class AddToCreateComments < ActiveRecord::Migration
+  def change
+      add_column :comments, :body, :text
+      add_column :comments, :user_id, :integer
+      add_column :comments, :post_id, :integer
+      add_column :comments, :created_at, :datetime
+      add_column :comments, :updated_at, :datetime
+  end
+end
