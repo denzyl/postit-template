b3227c8d11dc6bb07f2cdedde351b017f61dfbc9
Denzyl Balram <denzyl.balram@gmail.com>
2014-12-02 16:28:56 -0800

    Trying again.

--- db/migrate/20141021213056_add_to_create_comments.rb
+++ db/migrate/20141021213056_add_to_create_comments.rb
@@ -1,9 +1,12 @@
 class AddToCreateComments < ActiveRecord::Migration
-  def change
+  def up
       add_column :comments, :body, :text
       add_column :comments, :user_id, :integer
       add_column :comments, :post_id, :integer
       add_column :comments, :created_at, :datetime
       add_column :comments, :updated_at, :datetime
+  end
+
+  def down
   end
 end
